a1b0683e5fbc3268f9be5ac9ec2028740c90d721
Robert Ing <rmi22186@gmail.com>
2013-09-16 16:11:30 -0700

    include URL image add

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,14 +1,18 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image
+  attr_accessible :description, :image, :image_remote_url
 
   validates :description, presence: true
   validates :user_id, presence: true
   has_attached_file :image, styles: { medium: "100x100>" }
-  validates_attachment :image, presence: true,
+  validates_attachment :image,
   								content_type: { content_type: ['image/jpeg' 'image/jpg', 'image/png', 'image/gif'] },
   								size: { less_than: 5.megabytes }
 
   belongs_to :user
   has_attached_file :image, styles: { medium: "320x240>"}
 
+  def image_remote_url=(url_value)
+	 self.image = URI.parse(url_value) unless url_value.blank?
+	 super
+  end
 end
