00a451997d05f9d4bb0513c13593ce1e58210e29
Robert Ing <rmi22186@gmail.com>
2013-09-16 10:38:03 -0700

    Add Pins Associations

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '0431751f1d440fc2f3d116d14a3ea04c3bd5bef95263091e00611ecd54e5ff3a12638fc906306d99b49e7dc4432546ed8eaf6ab9fbcf4230c02e5c0d82dd9cbe'
