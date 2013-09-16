b4a9f8d7e7e10be2625b260ed73920bb0e5d5a1e
Robert Ing <rmi22186@gmail.com>
2013-09-16 16:57:40 -0700

    re-add back validate:presence for image

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,7 +4,7 @@ class Pin < ActiveRecord::Base
   validates :description, presence: true
   validates :user_id, presence: true
   has_attached_file :image, styles: { medium: "100x100>" }
-  validates_attachment :image,
+  validates_attachment :image, presence: true
   								content_type: { content_type: ['image/jpeg' 'image/jpg', 'image/png', 'image/gif'] },
   								size: { less_than: 5.megabytes }
 
